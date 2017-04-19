8dbfbfdd01ebc45eb49a23fe625824289311e5e9
Anu Vakkachen <avakkachen@phunware.com>
2017-04-19 15:24:29 -0500

    Updating for release 2.4.1. See CHANGELOG for details.

---
+++ SampleRewardedVisit/sample-rewarded-visit/app/src/main/java/com/phunware/advertising/sample/rvisit/RewardedVisitNotificationService.java
@@ -0,0 +1,18 @@
+package com.phunware.advertising.sample.rvisit;
+
+import android.support.v4.app.NotificationCompat;
+
+import com.phunware.locationmessaging.messages.services.NotificationCustomizationService;
+
+/**
+ * Customizes the notification displayed
+ */
+
+public class RewardedVisitNotificationService extends NotificationCustomizationService {
+
+    @Override
+    public void editNotification(NotificationCompat.Builder notificationBuilder) {
+        notificationBuilder.setSmallIcon(R.drawable.ic_stat_reward);
+        notificationBuilder.setColor(0xFF4081);
+    }
+}
